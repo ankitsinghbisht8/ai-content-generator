7679ad8e8a1f068413a2d676bf6bd138356ada6e
Ankit Singh Bisht <asbankit8@gmail.com>
2025-03-17 19:29:17 +0530

    Refactor: Store PostgreSQL URI securely using environment variables

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,6 +4,6 @@ export default {
   schema: "./utils/schema.tsx",
   dialect: 'postgresql',
   dbCredentials: {
-    url: 'postgresql://Ankit_owner:0dc1wexFQCoI@ep-dark-shadow-a5vp55z6-pooler.us-east-2.aws.neon.tech/Ankit?sslmode=require',
+    url: process.env.DATABASE_URL,
   }
 };
